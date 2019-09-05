220ab83b35d852fc1fc32e4f2eda4873042728d6
Greg Doud <gpdoud@gmail.com>
2019-09-05 14:57:39 -0400

    Added three read methods: 1) GetAll 2) GetByPk 3) Login

--- CSharp2SqlConsole/Program.cs
+++ CSharp2SqlConsole/Program.cs
@@ -1,9 +1,30 @@
 ﻿using System;
+using CSharp2SqlLibrary;
+using System.Diagnostics;
 
 namespace CSharp2SqlConsole {
     class Program {
+
+        void Run() {
+            var conn = new Connection(@"localhost\sqlexpress", "PrsDb7");
+            conn.Open();
+            Users.Connection = conn;
+            var userLogin = Users.Login("sa", "sa");
+            Console.WriteLine(userLogin);
+            var userFailedLogin = Users.Login("xx", "Xx");
+            Console.WriteLine(userFailedLogin?.ToString() ?? "Not found");
+            var users = Users.GetAll();
+            foreach(var u in users) {
+                Console.WriteLine(u);
+            }
+            var user = Users.GetByPk(2);
+            Debug.WriteLine(user);
+            var usernf = Users.GetByPk(222);
+            conn.Close();
+        }
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            var pgm = new Program();
+            pgm.Run();
         }
     }
 }
---
+++ CSharp2SqlLibrary/Users.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace CSharp2SqlLibrary {
+
+    public class Users {
+
+        public static Connection Connection { get; set; }
+
+        public static Users Login(string username, string password) {
+            var sql = "SELECT * from Users Where Username = @Username AND Password = @Password";
+            var sqlcmd = new SqlCommand(sql, Connection._Connection);
+            sqlcmd.Parameters.AddWithValue("@Username", username);
+            sqlcmd.Parameters.AddWithValue("@Password", password);
+            var reader = sqlcmd.ExecuteReader();
+            if(!reader.HasRows) {
+                reader.Close();
+                return null;
+            }
+            reader.Read();
+            var user = new Users();
+            LoadUserFromSql(user, reader);
+
+            reader.Close();
+            return user;
+        }
+
+        public static Users GetByPk(int id) {
+            var sql = "SELECT * from Users Where Id = @Id";
+            var sqlcmd = new SqlCommand(sql, Connection._Connection);
+            sqlcmd.Parameters.AddWithValue("@Id", id);
+            var reader = sqlcmd.ExecuteReader();
+            if(!reader.HasRows) {
+                reader.Close();
+                return null;
+            }
+            reader.Read();
+            var user = new Users();
+            LoadUserFromSql(user, reader);
+
+            reader.Close();
+            return user;
+        }
+
+        public static List<Users> GetAll() {
+            var sql = "SELECT * from Users;";
+            var sqlcmd = new SqlCommand(sql, Connection._Connection);
+            var reader = sqlcmd.ExecuteReader();
+            var users = new List<Users>();
+            while(reader.Read()) {
+                var user = new Users();
+                users.Add(user);
+
+                LoadUserFromSql(user, reader);
+            }
+            reader.Close();
+            return users;
+        }
+
+        private static void LoadUserFromSql(Users user, SqlDataReader reader) {
+            user.Id = (int)reader["Id"];
+            user.Username = reader["Username"].ToString();
+            user.Password = (string)reader["Password"];
+            user.Firstname = (string)reader["Firstname"];
+            user.Lastname = (string)reader["Lastname"];
+            user.Phone = reader["Phone"]?.ToString();
+            user.Email = reader["Email"]?.ToString();
+            user.IsAdmin = (bool)reader["IsAdmin"];
+            user.IsReviewer = (bool)reader["IsReviewer"];
+        }
+
+        public int Id { get; private set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool IsReviewer { get; set; }
+
+        public Users() {
+        }
+
+        public override string ToString() {
+            return $"Id={Id}, Username={Username}, Password={Password}, " +
+                    $"Name={Firstname} {Lastname}, Admin?={IsAdmin}, Reviewer?={IsReviewer}";
+        }
+    }
+}
